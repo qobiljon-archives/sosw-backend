d0a6d348cbd813c9d6afb998896e7f75406beb29
Qobiljon <qobiljon.toshnazarov@gmail.com>
2022-11-14 02:39:59 +0900

    urls has ending slash for APPEND_SLASH setting

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -158,3 +158,5 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'dashboard', 'staticfiles'),)
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+APPEND_SLASH = True
