0e3b8b6dd7b716e769f8cffbc26b49e966d8d8d0
Qobiljon <qobiljon.toshnazarov@gmail.com>
2022-12-01 04:21:04 +0900

    dq plots fixed

--- dashboard/views.py
+++ dashboard/views.py
@@ -45,29 +45,34 @@ def handle_dq_plot(request):
     users.extend(User.objects.filter(is_superuser = False))
 
   plots = list()
+  filenames = [
+    ('ppg', 16),
+    ('acc', 3),
+  ]
   for user in users:
-    filepath = f'{environ["DATA_DUMP_DIR"]}/{user.email}/ppg.csv'
-    if not exists(filepath): continue
-    lines_count = count_file_lines(filepath)
+    for filename, col_count in filenames:
+      filepath = f'{environ["DATA_DUMP_DIR"]}/{user.email}/{filename}.csv'
+      if not exists(filepath): continue
+      lines_count = count_file_lines(filepath)
 
-    names = ['ts'] + [chr(ord('a') + x) for x in range(16)]
-    df = pd.read_csv(
-      filepath,
-      names = names,
-      dtype = {x: int if x == 'ts' else float for x in names},
-      skiprows = max(0, lines_count - 540_000),
-    )
-    df = df[['ts', 'f']]
-    df = df[df.ts > df.iloc[-1].ts - 24*60*60*1000]
+      names = ['ts'] + [chr(ord('a') + x) for x in range(col_count)]
+      df = pd.read_csv(
+        filepath,
+        names = names,
+        dtype = {x: int if x == 'ts' else float for x in names},
+        skiprows = max(0, lines_count - 540_000),
+      )
+      df = df[['ts', 'a']]
+      df = df[df.ts > df.iloc[-1].ts - 24*60*60*1000]
 
-    df.ts = pd.to_datetime(df.ts, unit = 'ms', utc = True).map(lambda x: x.tz_convert('Asia/Seoul'))
-    df.set_index('ts', drop = True, inplace = True)
+      df.ts = pd.to_datetime(df.ts, unit = 'ms', utc = True).map(lambda x: x.tz_convert('Asia/Seoul'))
+      df.set_index('ts', drop = True, inplace = True)
 
-    df_resampled = df.resample('60S').count()
-
-    plots.append(
-      plotly.offline.plot(
-        figure_or_data = px.bar(df_resampled, title = user.email),
+      df_resampled = df.resample('60S').count().rename(columns = {'value': 'amount/minute'})
+      fig = px.bar(df_resampled, title = f'{user.email}, {filename}.csv')
+      fig.update_layout(xaxis_title = "Time", yaxis_title = "amount of data / minute")
+      plots.append(plotly.offline.plot(
+        figure_or_data = fig,
         auto_open = False,
         output_type = "div",
       ))
